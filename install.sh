8b6be256a95518082ff07d1759f68d6d6ab5bd57
andrewcgaitskell <andrew@gaitskell.com>
2025-01-15 09:21:12 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,4 +5,6 @@ apt install -y libxvidcore-dev libx264-dev libfontconfig1-dev libcairo2-dev
 apt install -y libgdk-pixbuf2.0-dev libpango1.0-dev libgtk2.0-dev libgtk-3-dev
 apt install -y libatlas-base-dev gfortran python3-dev python3-pip
 apt install -y libcamera-apps
+apt install -y samba
+
 
