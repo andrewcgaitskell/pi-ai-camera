0666b7608ec2283446ec4f9f2075f0655e349947
andrewcgaitskell <andrew@gaitskell.com>
2025-01-15 09:18:28 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,8 @@
 apt update
+apt install -y build-essential cmake git pkg-config libjpeg-dev libtiff-dev lib>
+apt install -y libavcodec-dev libavformat-dev libswscale-dev libv4l-dev
+apt install -y libxvidcore-dev libx264-dev libfontconfig1-dev libcairo2-dev
+apt install -y libgdk-pixbuf2.0-dev libpango1.0-dev libgtk2.0-dev libgtk-3-dev
+apt install -y libatlas-base-dev gfortran python3-dev python3-pip
 apt install -y libcamera-apps
 
