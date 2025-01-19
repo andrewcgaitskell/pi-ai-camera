4abbd00bc97de2988cbd9b88dd9628c310d46a1a
andrewcgaitskell <andrew@gaitskell.com>
2025-01-19 15:32:16 +0000

    Update asgi.py

--- app/asgi.py
+++ app/asgi.py
@@ -1,4 +1,11 @@
-from app import quart_app
+import hypercorn
+from hypercorn.config import Config
+from hypercorn.asyncio import serve
+from my_app import quart_app
 
-# Expose the ASGI application
-app = quart_app
+config = Config()
+config.bind = ["0.0.0.0:5000"]
+
+# Run the server
+import asyncio
+asyncio.run(serve(quart_app, config))
