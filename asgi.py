1f2077efabe152c762ab2bfad59aef594fffaefa
andrewcgaitskell <andrew@gaitskell.com>
2025-01-19 16:00:18 +0000

    Update asgi.py

--- asgi.py
+++ asgi.py
@@ -8,6 +8,7 @@ config.bind = ["0.0.0.0:5000"]
 
 # Set timeout to a custom value (e.g., 300 seconds for 5 minutes)
 config.timeout = 300
+config.http_timeout = 300
 
 # Run the server
 import asyncio
